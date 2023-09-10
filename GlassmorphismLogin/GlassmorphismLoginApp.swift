36d4dc80030e46c6df9950dd73fbcc3b942c0c9b
Zelyna Farrell <zsillas@yahoo.com>
2023-09-09 17:35:07 -0700

    Initial Commit

---
+++ GlassmorphismLogin/GlassmorphismLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassmorphismLoginApp.swift
+//  GlassmorphismLogin
+//
+//  Created by Zelyna Sillas on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct GlassmorphismLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
